ac0341e2fe383087c25f7baa67d36d72deb8cca8
Roland Arsenault <roland@ccom.unh.edu>
2019-10-17 18:19:54 -0400

    Finished decoding of all messages seen in log from first test run in Portsmouth harbor and added sending to ROS.

--- nodes/ais_node.py
+++ nodes/ais_node.py
@@ -6,13 +6,19 @@ import rospy
 from  sensor_msgs.msg import NavSatFix
 from sensor_msgs.msg import TimeReference
 from marine_msgs.msg import NavEulerStamped
+from marine_msgs.msg import Contact
+from marine_msgs.msg import Heartbeat
+from marine_msgs.msg import KeyValue
 import datetime
 import calendar
 import sys
+import ais.decoder
 
 def ais_listener(logdir=None):
     position_pub = rospy.Publisher('/base/position',NavSatFix,queue_size=10)
     timeref_pub = rospy.Publisher('/base/time_reference',TimeReference,queue_size=10)
+    ais_pub = rospy.Publisher('/base/ais/contacts',Contact,queue_size=10)
+    ais_raw_pub = rospy.Publisher('/base/ais/raw',Heartbeat,queue_size=10)
     rospy.init_node('ais')
     input_type = rospy.get_param('/ais/input_type')
     input_address = rospy.get_param('/ais/input','')
@@ -20,6 +26,8 @@ def ais_listener(logdir=None):
     input_port = int(rospy.get_param('/ais/input_port',0))
     output_port = int(rospy.get_param('/ais/output',0))
     output_address = rospy.get_param('/ais/output_address','<broadcast>')
+    
+    ais_decoder = ais.decoder.AISDecoder()
     
     if logdir is not None:
         logfile = file(logdir+'.'.join(datetime.datetime.utcnow().isoformat().split(':'))+'_ais.log','w')
@@ -53,48 +61,91 @@ def ais_listener(logdir=None):
         for nmea_in in nmea_ins:
             if logfile is not None:
                 logfile.write(datetime.datetime.utcnow().isoformat()+','+nmea_in+'\n')
-            nmea_parts = nmea_in.strip().split(',')
-            if len(nmea_parts):
-                #print nmea_parts
-                try:
-                    if nmea_parts[0][3:] == 'ZDA' and len(nmea_parts) >= 5:
-                        tref = TimeReference()
-                        tref.header.stamp = now
-                        hour = int(nmea_parts[1][0:2])
-                        minute = int(nmea_parts[1][2:4])
-                        second = int(nmea_parts[1][4:6])
-                        ms = int(float(nmea_parts[1][6:])*1000000)
-                        day = int(nmea_parts[2])
-                        month = int(nmea_parts[3])
-                        year = int(nmea_parts[4])
-                        zda = datetime.datetime(year,month,day,hour,minute,second,ms)
-                        tref.time_ref = rospy.Time(calendar.timegm(zda.timetuple()),zda.microsecond*1000)
-                        tref.source = 'ais'
-                        timeref_pub.publish(tref)
-                    if nmea_parts[0][3:] == 'GGA' and len(nmea_parts) >= 10:
-                        latitude = int(nmea_parts[2][0:2])+float(nmea_parts[2][2:])/60.0
-                        if nmea_parts[3] == 'S':
-                            latitude = -latitude
-                        longitude = int(nmea_parts[4][0:3])+float(nmea_parts[4][3:])/60.0
-                        if nmea_parts[5] == 'W':
-                            longitude = -longitude
-                        altitude = float(nmea_parts[9])
-                        nsf = NavSatFix()
-                        nsf.header.stamp = now
-                        nsf.header.frame_id = 'mobile_lab'
-                        nsf.latitude = latitude
-                        nsf.longitude = longitude
-                        nsf.altitude = altitude
-                        position_pub.publish(nsf)
-                    if nmea_parts[0][3:] == 'HDT' and len(nmea_parts) >= 2:
-                        heading = float(nmea_parts[1])
-                        nes = NavEulerStamped()
-                        nes.header.stamp = now
-                        nes.header.frame_id = 'mobile_lab'
-                        nes.orientation.heading = heading
-                        orientation_pub.publish(nes)
-                except ValueError:
-                    pass
+            if nmea_in.startswith('!AIVDM'):
+                ais_decoder.addNMEA(nmea_in.strip())
+                msgs = ais_decoder.popMessages()
+                for m in msgs:
+                    if m['type'] in (1,2,3,18,19): #position reports
+                        c = Contact()
+                        c.header.stamp = now
+                        c.mmsi = m['mmsi']
+                        if 'shipname' in ais_decoder.mmsi_db[m['mmsi']]:
+                            c.name = ais_decoder.mmsi_db[m['mmsi']]['shipname']
+                        if 'callsign' in ais_decoder.mmsi_db[m['mmsi']]:
+                            c.callsign = ais_decoder.mmsi_db[m['mmsi']]['callsign']
+                        c.position.latitude = m['lat']
+                        c.position.longitude = m['lon']
+                        if m['course'] is not None:
+                            c.cog = math.radians(m['course'])
+                        else:
+                            c.cog = -1
+                        if m['speed'] is not None:
+                            c.sog = m['speed']*0.514444 #knots to m/s
+                        else:
+                            c.sog = -1
+                        if m['heading'] is not None:
+                            c.heading = math.radians(m['heading'])
+                        else:
+                            c.heading = -1
+                        if 'to_bow' in ais_decoder.mmsi_db[m['mmsi']]:
+                            c.dimension_to_bow = ais_decoder.mmsi_db[m['mmsi']]['to_bow']
+                        if 'to_stern' in ais_decoder.mmsi_db[m['mmsi']]:
+                            c.dimension_to_stern = ais_decoder.mmsi_db[m['mmsi']]['to_stern']
+                        if 'to_port' in ais_decoder.mmsi_db[m['mmsi']]:
+                            c.dimension_to_port = ais_decoder.mmsi_db[m['mmsi']]['to_port']
+                        if 'to_starboard' in ais_decoder.mmsi_db[m['mmsi']]:
+                            c.dimension_to_stbd = ais_decoder.mmsi_db[m['mmsi']]['to_starboard']
+                        ais_pub.publish(c)
+                    raw = Heartbeat()
+                    for k,v in m.iteritems():
+                        raw.values.append(KeyValue(k,v))
+                    ais_raw_pub.publish(raw)
+                        
+                        
+                        
+            else:
+                nmea_parts = nmea_in.strip().split(',')
+                if len(nmea_parts):
+                    #print nmea_parts
+                    try:
+                        if nmea_parts[0][3:] == 'ZDA' and len(nmea_parts) >= 5:
+                            tref = TimeReference()
+                            tref.header.stamp = now
+                            hour = int(nmea_parts[1][0:2])
+                            minute = int(nmea_parts[1][2:4])
+                            second = int(nmea_parts[1][4:6])
+                            ms = int(float(nmea_parts[1][6:])*1000000)
+                            day = int(nmea_parts[2])
+                            month = int(nmea_parts[3])
+                            year = int(nmea_parts[4])
+                            zda = datetime.datetime(year,month,day,hour,minute,second,ms)
+                            tref.time_ref = rospy.Time(calendar.timegm(zda.timetuple()),zda.microsecond*1000)
+                            tref.source = 'ais'
+                            timeref_pub.publish(tref)
+                        if nmea_parts[0][3:] == 'GGA' and len(nmea_parts) >= 10:
+                            latitude = int(nmea_parts[2][0:2])+float(nmea_parts[2][2:])/60.0
+                            if nmea_parts[3] == 'S':
+                                latitude = -latitude
+                            longitude = int(nmea_parts[4][0:3])+float(nmea_parts[4][3:])/60.0
+                            if nmea_parts[5] == 'W':
+                                longitude = -longitude
+                            altitude = float(nmea_parts[9])
+                            nsf = NavSatFix()
+                            nsf.header.stamp = now
+                            nsf.header.frame_id = 'mobile_lab'
+                            nsf.latitude = latitude
+                            nsf.longitude = longitude
+                            nsf.altitude = altitude
+                            position_pub.publish(nsf)
+                        if nmea_parts[0][3:] == 'HDT' and len(nmea_parts) >= 2:
+                            heading = float(nmea_parts[1])
+                            nes = NavEulerStamped()
+                            nes.header.stamp = now
+                            nes.header.frame_id = 'mobile_lab'
+                            nes.orientation.heading = heading
+                            orientation_pub.publish(nes)
+                    except ValueError:
+                        pass
         
             
 
